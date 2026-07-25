623a3d4e2ef40f53835f658b5143de18e296b625
kato-yoshiharu <yoshiharu2580@gmail.com>
2026-07-25 11:52:42 +0900

    いくつかの不具合を修正

--- frontend/src/graphql/index.ts
+++ frontend/src/graphql/index.ts
@@ -1,7 +1,7 @@
 import { ApolloClient, InMemoryCache } from "@apollo/client";
 
 export const client = new ApolloClient({
-  uri: "http://localhost:8080",
+  uri: process.env.NEXT_PUBLIC_GRAPHQL_URL ?? "http://localhost:8080",
   cache: new InMemoryCache(),
   defaultOptions: {
     watchQuery: {
--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,6 +1,22 @@
 import { NextPage } from "next";
+import { useRouter } from "next/router";
+import { useEffect } from "react";
+import { useListRootPagesQuery } from "@/graphql/generated";
+import { routes } from "@/routes";
 
 const Home: NextPage = () => {
+  const router = useRouter();
+  const listRootPagesResult = useListRootPagesQuery();
+
+  useEffect(() => {
+    const data = listRootPagesResult.data;
+    if (data == null) return;
+    if (data.listRootPages.__typename !== "ListPages") return;
+    const firstPage = data.listRootPages.items[0];
+    if (firstPage == null) return;
+    router.replace(routes.notion.page.show(firstPage.id));
+  }, [listRootPagesResult.data, router]);
+
   return <div>Home</div>;
 };
 
