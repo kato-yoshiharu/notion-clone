38b774aa5935f08436c487eb5685c9379323518a
kato-yoshiharu <yoshiharu2580@gmail.com>
2026-08-25 09:53:31 +0900

    コードの修正

--- frontend/.eslintrc.js
+++ frontend/.eslintrc.js
@@ -3,5 +3,13 @@ module.exports = {
     // "@suimenkathemove/frontend-eslint-config",
     "./node_modules/@suimenkathemove/frontend-eslint-config",
   ],
-  ignorePatterns: ["src/graphql/generated", "src/wasm", "worker"],
+  ignorePatterns: [
+    "src/graphql/generated",
+    "src/wasm",
+    "worker",
+    "out",
+    ".next",
+    "storybook-static",
+    "coverage",
+  ],
 };
